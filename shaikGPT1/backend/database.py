fd6a1de4d379f3d07089b869f8cd1ccd6265e9f1
Shaik.skmj <vicksnews11@gmail.com>
2026-02-05 01:38:02 +0530

    Delete shaikGPT1 directory

--- shaikGPT1/backend/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://shaik:shaik@postgres:5432/shaikgpt")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
